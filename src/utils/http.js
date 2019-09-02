5fb888cc3f8410198ca881cf0773b3c82763ff0f
EgorRepnikov <egorepnikov@gmail.com>
2019-09-02 19:15:59 +0300

    Refactor HTTP client

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,17 +1,27 @@
-const BASE_URL = 'http://localhost:8080/'
+const API_URL = 'http://localhost:8080/'
+
+const token = typeof localStorage !== 'undefined' ?
+  localStorage.getItem('access-token') : undefined
 
 export function get(url) {
-  return fetch(BASE_URL + url, { method: 'GET' })
+  return wrap(url, { method: 'GET' })
 }
 
 export function post(url, body) {
-  return fetch(BASE_URL + url, { method: 'POST', body })
+  return wrap(url, { method: 'POST', body })
 }
 
 export function put(url, body) {
-  return fetch(BASE_URL + url, { method: 'PUT', body })
+  return wrap(url, { method: 'PUT', body })
 }
 
-export function del(url) {
-  return fetch(BASE_URL + url, { method: 'DELETE' })
+export function delete_(url) {
+  return wrap(url, { method: 'DELETE' })
+}
+
+function wrap(url, options = {}) {
+  if (token) {
+    options.headers['Authorization'] = token
+  }
+  return fetch(API_URL + url, options)
 }
