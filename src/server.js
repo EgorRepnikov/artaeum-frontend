7b225aa9f17b983f7ac7c3d1fd7c6c340158b22c
EgorRepnikov <egorepnikov@gmail.com>
2019-09-02 20:36:40 +0300

    Server Sessions

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
-import sirv from 'sirv'
 import { Dragonrend } from 'dragonrend'
+import session from 'express-session'
+import sessionFileStore from 'session-file-store'
 import compression from 'compression'
+import sirv from 'sirv'
 import * as sapper from '@sapper/server'
+
+const FileStore = sessionFileStore(session)
 
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
@@ -14,9 +18,23 @@ function wrapMiddleware(middleware) {
 
 app.middleware(
 	({ req }) => req.originalUrl = req.url,
+	wrapMiddleware(session({
+		secret: 'temp',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { maxAge: 31536000 },
+		store: new FileStore({ path: '.sessions' })
+	})),
+	({ req }) => {
+		if (typeof req.session.user === 'undefined') {
+			req.session.user = false
+		}
+	},
 	wrapMiddleware(compression({ threshold: 0 })),
 	wrapMiddleware(sirv('static', { dev })),
-	wrapMiddleware(sapper.middleware())
+	wrapMiddleware(sapper.middleware({
+		session: (req) => ({ user: req.session.user })
+	}))
 )
 
 app.toServer().listen(PORT, error => error && console.log(error))
