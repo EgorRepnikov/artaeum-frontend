96a104d862dea054255b5aecc580d2d0dae72d4d
EgorRepnikov <egorepnikov@gmail.com>
2019-09-03 18:18:11 +0300

    Update HTTP client

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,28 +3,28 @@ const API_URL = 'http://localhost:8080/'
 const token = typeof localStorage !== 'undefined' ?
   localStorage.getItem('access_token') : undefined
 
-export function get(url) {
-  return wrap(url, { method: 'GET' })
+export function get(url, fetch) {
+  return wrap(url, { method: 'GET' }, fetch)
 }
 
-export function post(url, body) {
-  return wrap(url, { method: 'POST', body: JSON.stringify(body) })
+export function post(url, body, fetch) {
+  return wrap(url, { method: 'POST', body: JSON.stringify(body) }, fetch)
 }
 
-export function put(url, body) {
-  return wrap(url, { method: 'PUT', body: JSON.stringify(body) })
+export function put(url, body, fetch) {
+  return wrap(url, { method: 'PUT', body: JSON.stringify(body) }, fetch)
 }
 
-export function delete_(url) {
-  return wrap(url, { method: 'DELETE' })
+export function delete_(url, fetch) {
+  return wrap(url, { method: 'DELETE' }, fetch)
 }
 
-function wrap(url, options = {}) {
+function wrap(url, options = {}, fetch_ = fetch) {
   if (token) {
     if (options.headers === undefined) {
       options.headers = {}
     }
     options.headers['Authorization'] = token
   }
-  return fetch(API_URL + url, options)
+  return fetch_(API_URL + url, options)
 }
