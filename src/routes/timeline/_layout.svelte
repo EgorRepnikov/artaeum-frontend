e42b2abf83b0125c312ff9ce5510b10e16131167
EgorRepnikov <egorepnikov@gmail.com>
2019-09-08 13:15:39 +0300

    Timeline layout component

---
+++ src/routes/timeline/_layout.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+  export function preload(_, { user }) {
+    if (!user) {
+      return this.error(401, 'Unauthorized')
+    }
+  }
+</script>
+
+<script>
+  export let segment
+</script>
+
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="tab-nav">
+        <a href="/timeline" class:active={!segment}>Article</a>
+        <a href="/timeline/posts" class:active={segment === 'posts'}>Posts</a>
+      </div>
+    </div>
+    <div class="row justify-content-md-center">
+      <slot></slot>
+    </div>
+  </div>
+</section>
