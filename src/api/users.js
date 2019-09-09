a0db6ac361be40a8cfb9841d9104f4c6822d9afc
EgorRepnikov <egorepnikov@gmail.com>
2019-09-09 18:20:55 +0300

    Change Password component

--- src/api/users.js
+++ src/api/users.js
@@ -10,7 +10,15 @@ export async function getUser(loginOrId, fetch_) {
   return response.status === 404 ? false : await response.json()
 }
 
-export async function saveUser(user, fetch_) {
-  const response = await post(`uaa/account`, user, fetch_)
+export async function saveUser(user) {
+  const response = await post('uaa/account', user)
+  return response.status === 200
+}
+
+export async function changePassword(password) {
+  const response = await fetch('uaa/account/change-password', {
+    method: 'POST',
+    body: password
+  })
   return response.status === 200
 }
---
+++ src/routes/settings/change-password.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { changePassword } from '../../api'
+
+  let success, error
+
+  let password = ''
+
+  async function submit() {
+    if (await changePassword(password)) {
+      success = true
+      password = ''
+    } else {
+      error = true
+    }
+  }
+</script>
+
+<svelte:head>
+	<title>Change password - Artaeum</title>
+</svelte:head>
+
+<div class="row">
+  <div class="col-md-12">
+    <h4>Change password - Artaeum</h4>
+    <hr>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    {#if error}
+      <div class="alert alert-danger">Error. Try again.</div>
+    {/if}
+    {#if success}
+      <div class="alert alert-success">Password has been updated.</div>
+    {/if}
+    <form on:submit|preventDefault={submit}>
+      <div class="form-group row">
+        <label for="password" class="col-4 col-form-label">New Password</label>
+        <div class="col-8">
+          <input id="password" class="form-control here" type="password" placeholder="New Password" bind:value={password}>
+        </div>
+      </div>
+      <div class="form-group row">
+        <div class="offset-4 col-8">
+          <button type="submit" class="btn btn-dark">Save</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
