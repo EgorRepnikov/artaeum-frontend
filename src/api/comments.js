e2e979541ff6b8e884cc2d944b08e741beaa4950
EgorRepnikov <egorepnikov@gmail.com>
2019-11-11 20:25:07 +0300

    Remove Svelte project

--- src/api/comments.js
+++
@@ -1,9 +0,0 @@
-import { get } from '../utils'
-
-export async function getComments(resourceType, resourceId, fetch_) {
-  const response = await get(
-    `media/comments?resourceType=${resourceType}&resourceId=${resourceId}`,
-    fetch_
-  )
-  return await response.json()
-}
